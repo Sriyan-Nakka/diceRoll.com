86b1fc145dbd81998a0e1c976007b0ff7361e4dd
Sriyan Nakka <sriyannakka@gmail.com>
2024-10-08 17:49:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,44 +7,109 @@ function diceRollFunction() {
     console.log(randNum);
     switch(randNum){
         case 1:
-            document.querySelector("#dice1").style.display = "block";
             document.querySelector("#dice2").style.display = "none";
             document.querySelector("#dice3").style.display = "none";
             document.querySelector("#dice4").style.display = "none";
             document.querySelector("#dice5").style.display = "none";
             document.querySelector("#dice6").style.display = "none";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice1").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 2:
             document.querySelector("#dice1").style.display = "none";
-            document.querySelector("#dice2").style.display = "block";
             document.querySelector("#dice3").style.display = "none";
             document.querySelector("#dice4").style.display = "none";
             document.querySelector("#dice5").style.display = "none";
             document.querySelector("#dice6").style.display = "none";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice2").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 3:
             document.querySelector("#dice1").style.display = "none";
             document.querySelector("#dice2").style.display = "none";
-            document.querySelector("#dice3").style.display = "block";
             document.querySelector("#dice4").style.display = "none";
             document.querySelector("#dice5").style.display = "none";
             document.querySelector("#dice6").style.display = "none";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice3").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 4:
             document.querySelector("#dice1").style.display = "none";
             document.querySelector("#dice2").style.display = "none";
             document.querySelector("#dice3").style.display = "none";
-            document.querySelector("#dice4").style.display = "block";
             document.querySelector("#dice5").style.display = "none";
             document.querySelector("#dice6").style.display = "none";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice4").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 5:
             document.querySelector("#dice1").style.display = "none";
             document.querySelector("#dice2").style.display = "none";
             document.querySelector("#dice3").style.display = "none";
             document.querySelector("#dice4").style.display = "none";
-            document.querySelector("#dice5").style.display = "block";
             document.querySelector("#dice6").style.display = "none";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice5").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 6:
             document.querySelector("#dice1").style.display = "none";
@@ -52,7 +117,20 @@ function diceRollFunction() {
             document.querySelector("#dice3").style.display = "none";
             document.querySelector("#dice4").style.display = "none";
             document.querySelector("#dice5").style.display = "none";
-            document.querySelector("#dice6").style.display = "block";
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is.";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is..";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "It is...";
+                        setTimeout(() => {
+                            document.querySelector("#result").textContent = "";
+                            document.querySelector("#dice6").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
     }
 }
